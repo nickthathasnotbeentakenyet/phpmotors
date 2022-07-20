783eb5e9d4ed5006288cd746e4435e4af8e1054e
Arkadii Lantukh <lan18029@byui.edu>
2022-07-20 16:51:21 +0300

    isset to session vars

--- snippets/header.php
+++ snippets/header.php
@@ -2,7 +2,8 @@
         <img src="/phpmotors/images/site/logo.png" alt="Delorian logo">
     </div>
     <div class="hright">
-        <?php if ($_SESSION['loggedin']) { echo '<a href="/phpmotors/accounts/index.php?action=admin" id="welcomeMessage">'; echo $_SESSION['clientData']['clientFirstname']; echo'</a>';}?>
-        <?php if (!$_SESSION['loggedin']) { echo '<a href="/phpmotors/accounts/index.php?action=login" id="myaccount">My Account</a>'; }?>
-        <?php if ($_SESSION['loggedin']) { echo '<a href="/phpmotors/accounts/index.php?action=logout" id="logout"> Logout</a>'; }?>
+        <?php if (isset($_SESSION['loggedin'])) { echo '<a href="/phpmotors/accounts/index.php?action=admin" id="welcomeMessage">'; 
+            if (isset($_SESSION['clientData']['clientFirstname'])) echo $_SESSION['clientData']['clientFirstname']; echo'</a>';}?>
+        <?php if (!isset($_SESSION['loggedin'])) { echo '<a href="/phpmotors/accounts/index.php?action=login" id="myaccount">My Account</a>'; }?>
+        <?php if (isset($_SESSION['loggedin'])) { echo '<a href="/phpmotors/accounts/index.php?action=logout" id="logout"> Logout</a>'; }?>
     </div>
